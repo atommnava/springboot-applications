49049cadaf0fe8ea5763bc7af37eac0453fc4ff9
atommnava <atommunoznava@gmail.com>
2026-01-13 17:00:14 -0600

    Clases

---
+++ src/main/java/com/informaticonfig/spring/app1/springboot_applications/controllers/EjemploController.java
@@ -0,0 +1,19 @@
+package com.informaticonfig.spring.app1.springboot_applications.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class EjemploController {
+    @GetMapping("/detalles_info")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    public String info() {
+        
+        return "Detalles_Info";
+    }
+}
