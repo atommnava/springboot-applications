42e114ffd99e7e8a8512086f1b6db1aa60f266fc
atommnava <atommunoznava@gmail.com>
2026-01-13 17:26:43 -0600

    Despliegue prueba

--- src/main/java/com/informaticonfig/spring/app1/springboot_applications/SpringbootApplicationsApplication.java
+++ src/main/java/com/informaticonfig/spring/app1/springboot_applications/SpringbootApplicationsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootApplicationsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplicationsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplicationsApplication.class, args);
+    }
 }
