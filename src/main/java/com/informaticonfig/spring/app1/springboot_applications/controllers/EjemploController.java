1d189e3205ad9e72cf5d008f9e89c74f81d43492
atommnava <atommunoznava@gmail.com>
2026-01-14 13:04:32 -0600

    Despliegue prueba

--- src/main/java/com/informaticonfig/spring/app1/springboot_applications/controllers/EjemploController.java
+++ src/main/java/com/informaticonfig/spring/app1/springboot_applications/controllers/EjemploController.java
@@ -1,15 +1,18 @@
 package com.informaticonfig.spring.app1.springboot_applications.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class EjemploController {
     @GetMapping("/detalles_info")
     
-    public String info() {
+    public String info(Model model) {
+        model.addAttribute("Título", "Servidor en línea");
+
         
         return "Detalles_Info";
     }
