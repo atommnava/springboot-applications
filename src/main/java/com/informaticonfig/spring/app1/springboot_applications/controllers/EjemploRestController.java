ae4f1c494b61c0b8fc2990f262f506e0368fe9cc
atommnava <atommunoznava@gmail.com>
2026-01-19 15:57:55 -0600

    Backend

---
+++ src/main/java/com/informaticonfig/spring/app1/springboot_applications/controllers/EjemploRestController.java
@@ -0,0 +1,20 @@
+package com.informaticonfig.spring.app1.springboot_applications.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class EjemploRestController {
+    @GetMapping("/detalles_info")
+    
+    public String info(Model model) {
+        model.addAttribute("titulo", "Servidor en línea");
+        model.addAttribute("servidor", "informaticonfig");
+        model.addAttribute("ip", "192.168.100.4");
+        
+        return "detalles_info";
+    }
+}
